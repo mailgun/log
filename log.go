78b8f10bcfc2a711eea92e4be5a07a21d35b8757
Roman Tkachenko <roman@mailgunhq.com>
2014-10-01 12:39:37 -0700

    Log severity.

--- log.go
+++ log.go
@@ -163,7 +163,7 @@ func Fatalf(format string, args ...interface{}) {
 
 func makeMessage(sev Severity, format string, args ...interface{}) string {
 	file, line := callerInfo()
-	return fmt.Sprintf("PID:%d [%s:%d] %s", pid, file, line, fmt.Sprintf(format, args...))
+	return fmt.Sprintf("PID:%d [%s:%d] %s %s", pid, file, line, sev, fmt.Sprintf(format, args...))
 }
 
 // Return stack traces of all the running goroutines.
